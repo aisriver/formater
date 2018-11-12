c7131535670fd2d11c1ad07c1dfc133963bf3480
jared <767882503@qq.com>
2018-11-12 14:35:17 +0800

    1.1.0

---
+++ README.md
@@ -0,0 +1,28 @@
+# formats
+简单的日期格式转化，通过正则的特殊判断，兼容大小写。 
+
+# how to use
+## step 1
+- npm install -g cnpm --registry=https://registry.npm.taobao.org
+- cnpm install formats --save
+
+## step 2
+最好在全局下引用
+<br/>
+import 'formats';
+
+- use
+
+<pre>
+new Date().formats('yyyy-mm-dd hh:mm:ss');
+2018-11 26:16
+
+new Date().formats('yyyy年mm月dd日 hh时mm分ss秒');
+2018年11月12日 14时26分52秒
+
+目前仅支持年、月、日、时、分、秒，
+季度与毫秒暂不支持
+</pre>
+
+### github
+[Jared](https://github.com/aisriver/formats.git)
---
+++ index.js
@@ -0,0 +1,55 @@
+; (function () {
+    "use strict"
+    var _global;
+    Date.prototype.formats = function (str) {
+        if (str && typeof str === 'string') {
+            function lessThanTen(n) {
+                if (Number(n) < 10) {
+                    return '0' + n;
+                } else {
+                    return n;
+                }
+            };
+            var transStr = str.toLowerCase();
+            var o = {
+                "(m+)(.)(d+|h+|m+)": this.getMonth() + 1, // 月
+                "d+": this.getDate(), // 日 
+                "h+": this.getHours(), // 时 
+                "m+": this.getMinutes(), // 分 
+                "s+": this.getSeconds(), // 秒 
+                // "q+": Math.floor((this.getMonth() + 3) / 3), // 季度 
+                // "s": this.getMilliseconds() // 毫秒 
+            };
+            if (/(y+)/.test(transStr)) {
+                transStr = transStr.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+            }
+            for (var k in o)
+                if (k === '(m+)(.)(d+|h+|m+)') {
+                    var mObj = transStr.match(k);
+                    var mbI = mObj.index, mStr = mObj[1];
+                    var rc = mStr.length === 1 ? lessThanTen(o[k]) : o[k];
+                    transStr = transStr.substring(0, mbI)
+                        + rc
+                        + transStr.substring(mbI + mStr.length, transStr.length);
+                } else {
+                    if (new RegExp("(" + k + ")").test(transStr)) {
+                        transStr = transStr.replace(RegExp.$1,
+                            (RegExp.$1.length === 1) ? lessThanTen(o[k])
+                                : o[k]
+                        );
+                    }
+                }
+            return transStr;
+        } else {
+            return '';
+        }
+    }
+    // _global = (function () { return this || (0, eval)('this'); }());
+    // if (typeof module !== "undefined" && module.exports) {
+    //     module.exports = Date;
+    // } else if (typeof define === "function" && define.amd) {
+    //     define(function () { return Date; });
+    // } else {
+    //     !('Date' in _global) && (_global.Date = Date);
+    // }
+}());
